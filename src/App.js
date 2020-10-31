bc639eaba60792dcae45a505a753fdfb4d69dc92
chankruze <chankruze@gmail.com>
2020-11-01 00:50:51 +0530

    Adds Download Component
    * Adds dummy download component
    * Removes cart for now
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import styles from "./App.module.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import About from "./components/About/About";
+// import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Store from "./components/Store/Store";
+import Download from "./components/Download/Download";
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/store" component={Store} />
-          <Route exact path="/cart" component={About} />
-          <Route exact path="/about" component={About} />
+          <Route exact path="/download" component={Download} />
+          {/* <Route exact path="/cart" component={Cart} /> */}
+          {/* <Route exact path="/about" component={About} /> */}
         </Switch>
         <Footer />
       </div>
---
+++ src/components/Download/Download.js
@@ -0,0 +1,15 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Sun Nov 01 2020 00:40:18 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from "react";
+import styles from "./Download.module.css";
+
+function Download() {
+  return <div></div>;
+}
+
+export default Download;
