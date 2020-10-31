d4ebd6bb3dc92a7240a1f9106e5c8a3931038d03
chankruze <chankruze@gmail.com>
2020-11-01 00:49:48 +0530

    Updates Navbar
    * Removes text-shadow from nav links
    * Adds download link
    * Removes cart for now
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/Navbar/NavItems.js
+++ src/components/Navbar/NavItems.js
@@ -5,8 +5,7 @@ Created: Fri Oct 30 2020 13:51:33 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
-import { RiDiscordFill } from "react-icons/ri";
-import { MdStore, MdShoppingCart, MdInfo } from "react-icons/md";
+import { MdStore, MdShoppingCart, MdInfo, MdFileDownload } from "react-icons/md";
 
 export const NavItems = [
   //   {
@@ -24,10 +23,16 @@ export const NavItems = [
     icon: <MdStore size={24}/>,
   },
   {
-    title: "Cart",
-    path: "/cart",
-    icon: <MdShoppingCart size={24}/>,
+    title: "Download",
+    path: "/download",
+    icon: <MdFileDownload size={24}/>,
   },
+
+  // {
+  //   title: "Cart",
+  //   path: "/cart",
+  //   icon: <MdShoppingCart size={24}/>,
+  // },
   // {
   //   title: "About",
   //   path: "/about",
