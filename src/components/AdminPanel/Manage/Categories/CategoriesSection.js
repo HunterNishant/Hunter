e1e1ba87a377583a4ee98fe9ba8da23d4703e50c
chankruze <chankruze@gmail.com>
2020-11-06 02:23:38 +0530

    Adds  blank Categories components
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/components/AdminPanel/Manage/Categories/CategoriesSection.js
@@ -0,0 +1,16 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Thu Nov 05 2020 03:05:40 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from "react";
+ // eslint-disable-next-line
+import styles from "./CategoriesSection.module.css";
+
+function CategoriesSection() {
+  return <div></div>;
+}
+
+export default CategoriesSection;
