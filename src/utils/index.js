136555f71f0314001aacf657c9abf770680d6647
chankruze <chankruze@gmail.com>
2020-11-08 02:46:30 +0530

    Adds Utility Functions for Fetching Data & Auth
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@ Created: Sat Oct 31 2020 05:23:29 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
+import axios from "axios";
 const CryptoJS = require("crypto-js"),
   sha256 = require("crypto-js/sha256"),
   hmacSHA256 = require("crypto-js/hmac-sha256");
@@ -43,8 +44,8 @@ export const generateSig = async () => {
     const localStorage = window.localStorage;
     // decode user, pass & gen cred hash
     const cred = sha256(
-      `"${eeDecrypt(localStorage.getItem("user"))}@${eeDecrypt(
-        localStorage.getItem("pass")
+      `"${eeDecrypt(localStorage.getItem("_getu"))}@${eeDecrypt(
+        localStorage.getItem("_getk")
       )}"`
     ).toString();
     // generate token
@@ -53,4 +54,68 @@ export const generateSig = async () => {
     const sig = hmacSHA256(token & date.toString(), cred).toString();
     resolve({ sig, timestamp: date });
   });
+};
+
+export const requestAuth = async (timestamp, sig) => {
+  // call auth url
+  const config = {
+    headers: { "x-hunter-signature": sig },
+  };
+
+  const { data: auth } = await axios
+    .post(
+      `${process.env.REACT_APP_BACKEND_PROD}/admin/auth`,
+      { timestamp },
+      config
+    )
+    .catch((err) => {
+      if (err.response.status === 401) {
+        // console.log("You're not authorized");
+        localStorage.clear();
+        return { auth: "failed" };
+      }
+    });
+
+  if (auth) {
+    if (auth.status === 69) {
+      return { auth: "success" };
+    } else {
+      // console.log("You're not authorized");
+      localStorage.clear();
+      return { auth: "failed" };
+    }
+  } else {
+    localStorage.clear();
+    return { auth: "failed" };
+  }
+};
+
+export const geekofiaAuth = async (user, pass) => {
+  const localStorage = window.localStorage;
+  localStorage.setItem("_getu", eeEncrypt(user));
+  localStorage.setItem("_getk", eeEncrypt(pass));
+  // console.log(generateSig());
+
+  const { sig, timestamp } = await generateSig();
+  const { auth } = await requestAuth(timestamp, sig);
+  return auth;
+};
+
+// call api for data
+export const fetchData = async (type) => {
+  const { sig, timestamp } = await generateSig();
+
+  const config = {
+    headers: { "x-hunter-signature": sig },
+  };
+
+  const { data: keys } = await axios
+    .post(
+      `${process.env.REACT_APP_BACKEND_PROD}/admin/fetch/${type}`,
+      { timestamp },
+      config
+    )
+    .catch((err) => console.log(err));
+
+  return keys || [];
 };
