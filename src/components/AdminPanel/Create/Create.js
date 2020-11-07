08ebddfda793982bd41931223c4834deb9996a53
chankruze <chankruze@gmail.com>
2020-11-08 02:48:10 +0530

    Starts working on Create component
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/components/AdminPanel/Create/Create.js
@@ -0,0 +1,18 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Fri Nov 06 2020 05:07:57 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from "react";
+import styles from "./Create.module.css";
+
+// const defaultSection = {
+//   name: "",
+//   id: 0,
+// };
+
+export const Create = () => {
+  return <div className={styles.create_root}>div</div>;
+};
---
+++ src/components/AdminPanel/Create/CreateKey.js
@@ -0,0 +1,13 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Fri Nov 06 2020 04:48:19 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from "react";
+import styles from "./CreateKey.module.css";
+
+export const CreateKey = () => {
+  return <div className={styles.new_key_root}></div>;
+};
