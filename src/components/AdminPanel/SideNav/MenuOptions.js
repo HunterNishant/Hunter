08b598b5f740db6a12ae7e042705569fe00f0155
chankruze <chankruze@gmail.com>
2020-11-08 02:42:40 +0530

    Merges & Updates SideNavbar in Admin Page
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/AdminPanel/SideNav/MenuOptions.js
+++ src/components/AdminPanel/SideNav/MenuOptions.js
@@ -5,14 +5,17 @@ Created: Tue Nov 03 2020 22:33:09 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
+// eslint-disable-next-line
 import { MdDashboard, MdBuild, MdCreate } from "react-icons/md";
+// eslint-disable-next-line
+import { RiLogoutBoxLine } from "react-icons/ri";
 
 export const MenuOptions = [
-  {
-    icon: <MdDashboard />,
-    link: "/admin/dashboard",
-    name: "dashboard",
-  },
+  // {
+  //   icon: <MdDashboard />,
+  //   link: "/admin/dashboard",
+  //   name: "dashboard",
+  // },
   {
     icon: <MdBuild />,
     link: "/admin/manage",
@@ -20,7 +23,7 @@ export const MenuOptions = [
   },
   {
     icon: <MdCreate />,
-    link: "/admin/new",
+    link: "/admin/add",
     name: "add",
   },
 ];
--- src/components/AdminPanel/SideNav/SideNav.js
+++ src/components/AdminPanel/SideNav/SideNav.js
@@ -5,68 +5,28 @@ Created: Tue Nov 03 2020 20:39:45 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
-import React, { useState } from "react";
-// import { MdMenu, MdClose } from "react-icons/md";
-import { Link } from "react-router-dom";
 import { MenuOptions } from "./MenuOptions";
 import styles from "./SideNav.module.css";
 
-function SideNav() {
-  // const [isOpened, setIsOpened] = useState(false);
-  const [activeMenu, setActiveMenu] = useState("dashboard");
-
-  // const toggleIsOpened = () => {
-  //   setIsOpened(!isOpened);
-  // };
+function SideNav(props) {
+  const { activeMenu, setActiveMenu } = props;
 
   return (
     <div className={styles.sidenav_root}>
-      {/* <div className={styles.menu_btn} onClick={toggleIsOpened}>
-        {isOpened ? <MdClose size={32} /> : <MdMenu size={32} />}
-      </div> */}
-
-      {/* {!isOpened && (
-        <div className={styles.menu_options_min}>
-          {MenuOptions &&
-            MenuOptions.map((option, i) => (
-              <Link
-                key={i}
-                to={option.link}
-                onClick={() => setActiveMenu(option.name)}
-                className={`${styles.menu_option_min} ${
-                  activeMenu === option.name ? `${styles.active}` : ""
-                }`}
-              >
-                {option.icon}
-              </Link>
-            ))}
-        </div>
-      )} */}
-
       <div className={styles.menu_options_min}>
         {MenuOptions &&
           MenuOptions.map((option, i) => (
-            <Link
+            <div
               key={i}
-              to={option.link}
               onClick={() => setActiveMenu(option.name)}
               className={`${styles.menu_option_min} ${
                 activeMenu === option.name ? `${styles.active}` : ""
               }`}
             >
               {option.icon}
-            </Link>
+            </div>
           ))}
       </div>
-
-      {/* {isOpened && (
-        <div className={styles.menu_options_full}>
-          {MenuOptions &&
-            MenuOptions.map((option, i) => (
-              <MenuOption key={i} data={option} />
-            ))}
-        </div>
-      )} */}
     </div>
   );
 }
