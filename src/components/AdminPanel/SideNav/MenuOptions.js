7166cf72b820dbcae2b12299d3c08b4582752e02
chankruze <chankruze@gmail.com>
2020-11-09 04:24:57 +0530

    Updates SideNav Options & Position
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/AdminPanel/SideNav/MenuOptions.js
+++ src/components/AdminPanel/SideNav/MenuOptions.js
@@ -5,10 +5,7 @@ Created: Tue Nov 03 2020 22:33:09 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
-// eslint-disable-next-line
-import { MdDashboard, MdBuild, MdCreate } from "react-icons/md";
-// eslint-disable-next-line
-import { RiLogoutBoxLine } from "react-icons/ri";
+import { MdAddCircleOutline, MdEdit } from "react-icons/md";
 
 export const MenuOptions = [
   // {
@@ -17,13 +14,13 @@ export const MenuOptions = [
   //   name: "dashboard",
   // },
   {
-    icon: <MdBuild />,
+    icon: <MdEdit />,
     link: "/admin/manage",
     name: "manage",
   },
   {
-    icon: <MdCreate />,
-    link: "/admin/add",
-    name: "add",
+    icon: <MdAddCircleOutline />,
+    link: "/admin/create",
+    name: "create",
   },
 ];
