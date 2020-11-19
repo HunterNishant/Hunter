2b07baded2571691113155890ba723637798cf91
chankruze <chankruze@gmail.com>
2020-11-19 22:58:42 +0530

    Parses Description & Renders Youtube Videos & Images
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/Download/DownloadPostFull/DownloadPostFull.js
+++ src/components/Download/DownloadPostFull/DownloadPostFull.js
@@ -7,10 +7,21 @@ Copyright (c) Geekofia 2020 and beyond
 
 import { MdClose } from "react-icons/md";
 import Moment from "react-moment";
+import ReactPlayer from "react-player";
 import styles from "./DownloadPostFull.module.css";
 
 export const DownloadPostFull = ({ selectedPost, close }) => {
-  const { title, sub, date, image, description, link, tags } = selectedPost;
+  const {
+    title,
+    sub,
+    date,
+    // images,
+    description,
+    // extLinks,
+    downloadLink,
+    // ytLinks,
+    tags,
+  } = selectedPost;
   return (
     <div className={styles.download_full_root}>
       <div className={styles.post_wrapper}>
@@ -30,17 +41,77 @@ export const DownloadPostFull = ({ selectedPost, close }) => {
         {/* sub title */}
         {sub && <h3 className={styles.sub_title}>{sub}</h3>}
         {/* description */}
-        <div className={styles.description}>
-          {/* image */}
-          {image && <img src={image} alt="Screenshot" />}
-          {description && description.replace(/\\n/g, "\n")}
+        <div className={styles.description_wrapper}>
+          {/* images */}
+          {/* {images && (
+            <div className={styles.screenshots_wrapper}>
+              {images.map((img, i) => (
+                <img key={i} src={img} alt={`Screenshot ${i}`} />
+              ))}
+            </div>
+          )} */}
+
+          {/* descriptions */}
+          {description && (
+            <div className={styles.description}>
+              {description.split(" ").map((word) => {
+                const urlRegex = /(https?:\/\/[^\s]+)/g;
+                const imgRegex = /\.(?:jpe?g|gif|png)$/i;
+                if (word.match(urlRegex)) {
+                  if (word.includes("youtu"))
+                    return (
+                      <ReactPlayer
+                        url={word}
+                        height={280}
+                        width={480}
+                        style={{
+                          marginTop: "1rem",
+                          marginBottom: "1rem",
+                          alignSelf: "center",
+                          padding: "0.5rem",
+                        }}
+                      />
+                    );
+
+                  if (word.match(imgRegex))
+                    return <img src={word} alt={word} />;
+
+                  return <a href={word}>{word}</a>;
+                } else {
+                  return `${word}${" "}`;
+                }
+              })}
+            </div>
+          )}
+
+          {/* links */}
+          {/* {extLinks && (
+            <div className={styles.ext_links_wrapper}>
+              <h3>Links</h3>
+              {extLinks.map((extLink, i) => (
+                <a href={extLink.link} target="_blank" rel="noreferrer">
+                  {extLink.title}
+                </a>
+              ))}
+            </div>
+          )} */}
+
+          {/* YT videos */}
+          {/* {ytLinks && (
+            <div className={styles.yt_videos_wrapper}>
+              <h3>Videos</h3>
+              {ytLinks.map((ytLink, i) => (
+                <ReactPlayer url={ytLink} height={250} width={350} />
+              ))}
+            </div>
+          )} */}
         </div>
         {/* download & discuss */}
         <div className={styles.action_group}>
           <a href="https://discord.gg/EfUxsY7" className={styles.a_discuss}>
             Discuss
           </a>
-          <a href={link} className={styles.btn_download}>
+          <a href={downloadLink} className={styles.btn_download}>
             Download
           </a>
         </div>
